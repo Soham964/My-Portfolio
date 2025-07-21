14cec91073e04acd2f8ebf7dbbfdb19777fded00
Soham964 <sdrixon964@gmail.com>
2025-07-21 07:03:42 +0530

    Update GitHub link in About page to correct profile: Soham964

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -238,7 +238,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://github.com/SohamDatta" target="_blank" rel="noopener noreferrer" className="w-full lg:w-auto">
+              <a href="https://github.com/Soham964" target="_blank" rel="noopener noreferrer" className="w-full lg:w-auto">
                 <button
                   data-aos="fade-up"
                   data-aos-duration="700"
